e6455c7bdaab3bc321b851213ba3652385bf68f7
serg_bloim <email>
2019-06-03 19:37:43 -0700

    untrack venv

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cheat-engine',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='spambox',
+    author_email='',
+    description=''
+)
