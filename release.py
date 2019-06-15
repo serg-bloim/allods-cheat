a96403100093c08042c62948562b0cec53902e8d
serg_bloim <email>
2019-06-15 12:24:59 -0700

    improve release process

--- release.py
+++ release.py
@@ -51,7 +51,7 @@ def release():
     print(repo)
     rel: GitRelease = repo.create_git_release(tag=new_release_version, name=new_release_version, message='new release',target_commitish=sha)
     rel.upload_asset(wheel_file)
-    rel.upload_asset('upgrade.bat')
+    rel.upload_asset(get_upgrade_filename(new_release_version))
     rel.upload_asset('static.zip')
 
 release()
